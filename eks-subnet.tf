804f0a7f958dbe44f1a615aee8a93004b5407c30
Kushan Thamel <kushan.thamel@gmail.com>
2023-09-16 18:53:01 +0530

    corrected the referenced vpc_id

--- eks-subnet.tf
+++ eks-subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "kthamel-eks-subnet-0" {
-  vpc_id                  = "{aws_vpc.kthaml-eks-vpc.id}"
+  vpc_id                  = aws_vpc.kthamel-eks-vpc.id
   cidr_block              = "172.32.0.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
@@ -8,7 +8,7 @@ resource "aws_subnet" "kthamel-eks-subnet-0" {
 }
 
 resource "aws_subnet" "kthamel-eks-subnet-1" {
-  vpc_id            = "{aws_vpc.kthaml-eks-vpc.id}"
+  vpc_id            = aws_vpc.kthamel-eks-vpc.id
   cidr_block        = "172.32.1.0/24"
   availability_zone = "us-east-1a"
 
@@ -16,7 +16,7 @@ resource "aws_subnet" "kthamel-eks-subnet-1" {
 }
 
 resource "aws_subnet" "kthamel-eks-subnet-2" {
-  vpc_id            = "{aws_vpc.kthaml-eks-vpc.id}"
+  vpc_id            = aws_vpc.kthamel-eks-vpc.id
   cidr_block        = "172.32.2.0/24"
   availability_zone = "us-east-1b"
 
@@ -24,11 +24,9 @@ resource "aws_subnet" "kthamel-eks-subnet-2" {
 }
 
 resource "aws_subnet" "kthamel-eks-subnet-3" {
-  vpc_id            = "{aws_vpc.kthaml-eks-vpc.id}"
+  vpc_id            = aws_vpc.kthamel-eks-vpc.id
   cidr_block        = "172.32.3.0/24"
   availability_zone = "us-east-1c"
 
   tags = local.common_tags
 }
-
-
