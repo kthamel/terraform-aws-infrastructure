e4f6b72f28ca1ce5f493597088078e1d0d894ed8
kthamel <66661262+kthamel@users.noreply.github.com>
2023-09-18 18:44:10 +0530

    modified the subnet association (#13)
    * required basic configuration was added
    
    * modified the routing tables
    
    * modified the eks vpc configurations
    
    * modified the eks vpc configurations
    
    * oidc configuration was modified
    
    * syntax issue was corrected on oidc configurations
    
    * modified the subnet ids on eks
    
    * modified the subnet ids on eks
    
    * modified the service account name
    
    * corrected the referenced vpc_id
    
    * resolved the malfunction error 01
    
    * removed the unnecessary last line
    
    * modifed the iam policy statement
    
    * fixed the malfunction files
    
    * terraform fmt was executed
    
    * role names were corrected
    
    * requested changes were added
    
    * modified the policies
    
    * fixed the malfunctioned policy
    
    * fixed the variables
    
    * modified the configurations
    
    * few configurations were rolled back
    
    * modified the public subnet id
    
    * modified the NAT GQ association subnet
    
    * modified the subnet association

--- eks-vpc.tf
+++ eks-vpc.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "kthamel-eks-vpc" {
-  cidr_block = "172.32.0.0/16"
+  cidr_block                       = "172.32.0.0/16"
+  assign_generated_ipv6_cidr_block = false
 
   tags = local.common_tags
 }
