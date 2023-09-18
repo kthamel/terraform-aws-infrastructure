6efc4c274ffa974bb1375fbe9d4da3bc7e4ca2bd
Kushan Thamel <kushan.thamel@gmail.com>
2023-09-18 21:13:13 +0530

    modified the routing table configurations

--- eks-routing-table.tf
+++ eks-routing-table.tf
@@ -1,9 +1,9 @@
 resource "aws_route_table" "kthamel-eks-private-routing" {
-  vpc_id = "{aws_vpc.kthaml-eks-vpc.id}"
+  vpc_id = aws_vpc.kthamel-eks-vpc.id
   route = [
     {
       cidr_block                 = "0.0.0.0/0"
-      nat_gateway_id             = "{aws_nat_gateway.kthamel-eks-nat-gw.id}"
+      nat_gateway_id             = aws_nat_gateway.kthamel-eks-nat-gw.id
       carrier_gateway_id         = ""
       core_network_arn           = ""
       destination_prefix_list_id = ""
@@ -21,16 +21,16 @@ resource "aws_route_table" "kthamel-eks-private-routing" {
 }
 
 resource "aws_route_table_association" "kthamel-eks-rt-association-1" {
-  subnet_id      = "{aws_subnet.kthamel-eks-subnet-1.id}"
+  subnet_id      = aws_subnet.kthamel-eks-subnet-1.id
   route_table_id = aws_route_table.kthamel-eks-private-routing.id
 }
 
 resource "aws_route_table_association" "kthamel-eks-rt-association-2" {
-  subnet_id      = "{aws_subnet.kthamel-eks-subnet-2.id}"
+  subnet_id      = aws_subnet.kthamel-eks-subnet-2.id
   route_table_id = aws_route_table.kthamel-eks-private-routing.id
 }
 
 resource "aws_route_table_association" "kthamel-eks-rt-association-3" {
-  subnet_id      = "{aws_subnet.kthamel-eks-subnet-3.id}"
+  subnet_id      = aws_subnet.kthamel-eks-subnet-3.id
   route_table_id = aws_route_table.kthamel-eks-private-routing.id
 }
