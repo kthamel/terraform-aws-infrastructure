4624371ce942c04c1b7ffb20cc0796eb62605204
Kushan Thamel <kushan.thamel@gmail.com>
2023-09-19 13:39:00 +0530

    additional worker node was added

--- eks-nodes.tf
+++ eks-nodes.tf
@@ -45,7 +45,7 @@ resource "aws_eks_node_group" "private-nodes" {
   capacity_type  = "ON_DEMAND"
   instance_types = ["t2.micro"]
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     min_size     = 0
     max_size     = 2
   }
